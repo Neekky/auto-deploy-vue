58c0d3d9061411480886d488ddbbde075a9607a3
CengChao <chao1.zeng@aihuishou.com>
2022-03-06 17:08:24 +0800

    feat: 修改脚本

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,10 @@ echo "删除旧文件"
 # 删除 ngnix 指向的文件夹下得文件
 rm -rf /home/www/*
 
+echo "将打包好的文件复制至www文件夹"
 # 将打包好的文件复制过去
 mv /tmp/auto-deploy-vue/dist/*  /home/www
+
+echo "删除克隆的代码"
 # 删除 clone 的代码
 rm -rf /tmp/auto-deploy-vue
